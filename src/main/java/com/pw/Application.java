9f9e1095a2ce378a27f01d22052d693b70975c8d
x003093 <francis.garberiii@prattwhitney.com>
2022-10-06 11:45:37 -0400

    Initial version of hello world Spring App.

---
+++ src/main/java/com/pw/Application.java
@@ -0,0 +1,31 @@
+package com.pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    // http://localhost:8080/v1.0.0/services/getHelloMessage
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        
+        System.out.println("/************************************************************************************/");
+        System.out.println("/*                                                                                  */");
+        System.out.println("/*                              Hello, world...                                     */");
+        System.out.println("/*                                                                                  */");
+        System.out.println("/*          SpringApplicationBuilder.configure(SpringApplicationBuilder):           */");
+        System.out.println("/*                                                                                  */");
+        System.out.println("/************************************************************************************/");
+        
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
