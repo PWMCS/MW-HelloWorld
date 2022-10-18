e578d3860b104390f30fad7cea62703e79ed0cef
x003093 <francis.garberiii@prattwhitney.com>
2022-10-18 17:19:30 -0400

    Updated the controller to state that it explicitly "produces" a text / html value.

--- src/main/java/com/pw/Controller.java
+++ src/main/java/com/pw/Controller.java
@@ -2,17 +2,20 @@ package com.pw;
 
 import java.util.Date;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping(value = "/v1.0.0/services/getHelloMessage")
-    public ResponseEntity<String> fetchAllContactUs() throws Exception {
+    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping(value = "/v1.0.0/services/getHelloMessage", produces = { MediaType.TEXT_HTML_VALUE })
+    public String fetchAllContactUs() throws Exception {
 
-        return ResponseEntity.ok("Hello, world... : " + new Date());
+        return "Hello, world... : " + new Date();
 
     }
 
